770fe041da93e8a2bce06430eb3625164416f5e0
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-08-14 15:28:57 +0200

    chore: update to marigold 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Box, Button, MarigoldProvider } from "@marigold/components";
-import theme from "@marigold/theme-b2b";
+import React from 'react';
+import { Button, Inset, MarigoldProvider } from '@marigold/components';
+import theme from '@marigold/theme-b2b';
 
 export const App = () => (
   <MarigoldProvider theme={theme}>
-    <Box css={{ p: "small" }}>
-      <Button variant="primary" onClick={() => alert("Hey there!")}>
+    <Inset space={10}>
+      <Button variant="primary" onClick={() => alert('Hey there!')}>
         Hello World
       </Button>
-    </Box>
+    </Inset>
   </MarigoldProvider>
 );
