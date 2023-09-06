56235bcd12e7b6958205358e74309a7a6e45beb6
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-09-06 10:50:19 +0200

    clean up tailwind content configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,6 @@ import { preset } from '@marigold/theme-b2b/preset';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    ...preset.content,
-    './node_modules/@marigold/system/dist/*.js',
-    './node_modules/@marigold/components/dist/*.js',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: [...preset.content, './src/**/*.{js,ts,jsx,tsx}'],
   presets: [preset],
 };
