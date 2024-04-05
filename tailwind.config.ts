77083fa0b04f8ae089fe941fb807b09fb93b7f6a
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-04-05 14:28:33 +0200

    chore: update to latest and make core the default theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
-import { preset } from '@marigold/theme-b2b/preset';
+import { preset } from '@marigold/theme-core/preset';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  corePlugins: {
+    preflight: true,
+  },
   content: [...preset.content, './src/**/*.{js,ts,jsx,tsx}'],
   presets: [preset],
 };
